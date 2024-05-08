6eb974b7c0dd72b10c742b81fccfaaf8f1ce793e
Bumfuzzle-github <135697664+Bumfuzzle-github@users.noreply.github.com>
2024-05-08 09:00:54 +0300

    Update primitive.cpp
    code obnovlen

--- serialization/src/primitive.cpp
+++ serialization/src/primitive.cpp
@@ -2,6 +2,8 @@
 #include "../include/core.h"
 #include <stdint.h>
 
+#include <memory>
+
 namespace ObjectModel
 {
 
@@ -11,41 +13,43 @@ namespace ObjectModel
 	}
 
 
-	void Primitive::pack(std::vector<uint8_t>& buffer, int16_t& iterator)
+	void Primitive::pack(std::vector<uint8_t>& buffer, int16_t& iterator) const
 	{
-		Core::encode<uint8_t>(buffer, iterator, wrapper);
-		Core::encode<int16_t>(buffer, iterator, nameLength);
-		Core::encode<std::string>(buffer, iterator, name);
-		Core::encode<uint8_t>(buffer, iterator, type);
-		Core::encode<uint8_t>(buffer, iterator, *data);
-		Core::encode<int32_t>(buffer, iterator, size);
+		 Core::encode<uint8_t>(buffer, iterator, wrapper);
+                 Core::encode<int16_t>(buffer, iterator, nameLength);
+                 Core::encode<std::string>(buffer, iterator, name);
+                 Core::encode<uint8_t>(buffer, iterator, type);
+
+                 if (data) {
+                   Core::encode(buffer, iterator, *(*data));
+                   Core::encode<int32_t>(buffer, iterator, static_cast<int32_t>((*data)->size()));
+                }
 		
 	}
 
 
-	Primitive Primitive::unpack(const std::vector<uint8_t>& buffer, int16_t& it)
+	static Primitive Primitive::unpack(const std::vector<uint8_t>& buffer, int16_t& it)
 	{
-		Primitive p;
+             Primitive p;
 
-		p.wrapper = Core::decode<uint8_t>(buffer, it);
-		p.nameLength = Core::decode<int16_t>(buffer, it);
-		p.name = Core::decode<std::string>(buffer, it);
-		p.type = Core::decode<uint8_t>(buffer, it);
-		p.data = new std::vector<uint8_t>(getTypeSize((Type)p.type));
-		Core::decode(buffer, it, *p.data);
-		p.size = Core::decode<int32_t>(buffer, it);
+             p.wrapper = Core::decode<uint8_t>(buffer, it);
+             p.nameLength = Core::decode<int16_t>(buffer, it);
+             p.name = Core::decode<std::string>(buffer, it);
+             p.type = Core::decode<uint8_t>(buffer, it);
 
+             int dataSize = p.getTypeSize(static_cast<Type>(p.type));
+             p.data = std::make_unique<std::vector<uint8_t>>(dataSize);
 
-		return p;
+             Core::decode(buffer, it, *p.data);
+             p.size = Core::decode<int32_t>(buffer, it);
+
+             return p;		
 	}
 
 
-	std::vector<uint8_t> Primitive::getData()
-	{
-		return *data;
-	}
-
-
-	
-
+      std::vector<uint8_t> getData() const {
+          if (data) 
+              return *(*data);
+          return {};
+      }
 }
