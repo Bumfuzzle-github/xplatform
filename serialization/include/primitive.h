a783c11a6233a86db67407d7e1c82a4b629bf830
Bumfuzzle-github <135697664+Bumfuzzle-github@users.noreply.github.com>
2024-05-08 09:26:57 +0300

    Update primitive.h
    kod pofiksen

--- serialization/include/primitive.h
+++ serialization/include/primitive.h
@@ -10,23 +10,23 @@ namespace ObjectModel
 	{
 	private:
 		uint8_t type = 0;
-		std::vector<uint8_t>* data = nullptr;
-	private:
-		Primitive();
+		std::unique_ptr<std::vector<uint8_t>> data;
+                Primitive() = default;
 	public:
 		template<typename T>
 		static std::unique_ptr<Primitive> create(std::string name, Type type, T value)
 		{
-			std::unique_ptr<Primitive> p(new Primitive());
-			p->setName(name);
-			p->wrapper = static_cast<uint8_t>(Wrapper::PRIMITIVE);
-			p->type = static_cast<uint8_t>(type);
-			p->data = new std::vector<uint8_t>(sizeof value);
-			p->size += (int32_t)p->data->size();
-			int16_t iterator = 0;
-			Core::encode<T>(*p->data, iterator, value);
+			auto p = std::make_unique<Primitive>();
+                        p->setName(name);
+                        p->wrapper = static_cast<uint8_t>(Wrapper::PRIMITIVE);
+                        p->type = static_cast<uint8_t>(type);
+                        p->data = std::make_unique<std::vector<uint8_t>>(sizeof(T));
 
-			return p;
+                        int16_t iterator = 0;
+                        Core::encode<T>(*p->data, iterator, value);
+
+                         p->size += static_cast<int32_t>(p->data->size());
+                         return p;
 		}
 
 		void pack(std::vector<uint8_t>&, int16_t&);
